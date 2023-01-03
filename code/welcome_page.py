f22a1e5c9af4e5e66dce1c31a3fa086f9110a56d
Matvey <kopo6ku@yandex.ru>
2023-01-03 17:56:59 +0300

    First Commit

--- code/welcome_page.py
+++ code/welcome_page.py
@@ -2,6 +2,8 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtGui import QIcon, QCursor, QFontDatabase, QFont
+import subprocess
+import os
 
 
 class Welcome_page(QMainWindow):
@@ -38,61 +40,59 @@ class Welcome_page(QMainWindow):
         self.pushButton1.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.pushButton1.clicked.connect(self.termination)
         self.pushButton1.setStyleSheet('border: 0px;')
-        self.pushButton6 = QPushButton('', self)
-        self.pushButton6.move(00, 700)
-        self.pushButton6.setIcon(QIcon('../data/Информация.svg'))
-        self.pushButton6.clicked.connect(self.info)
-        self.pushButton6.setStyleSheet('border: 0px')
-        self.pushButton6.setIconSize(QSize(80, 80))
-        self.pushButton6.adjustSize()
-        id = QFontDatabase.addApplicationFont('../data/Шрифт.TTF')
-        families = QFontDatabase.applicationFontFamilies(id)
+        self.pushButton5 = QPushButton('', self)
+        self.pushButton5.move(00, 700)
+        self.pushButton5.setIcon(QIcon('../data/Информация.svg'))
+        self.pushButton5.clicked.connect(self.info)
+        self.pushButton5.setStyleSheet('border: 0px')
+        self.pushButton5.setIconSize(QSize(80, 80))
+        self.pushButton5.adjustSize()
+        id1 = QFontDatabase.addApplicationFont('../data/Шрифт.TTF')
+        families = QFontDatabase.applicationFontFamilies(id1)
         self.label = QLabel('MAZE', self)
         self.label.setFont(QFont(families[0], 98))
         self.label.move(205, 100)
         self.label.adjustSize()
-        id = QFontDatabase.addApplicationFont('../data/Шрифт2.ttf')
-        families = QFontDatabase.applicationFontFamilies(id)
+        id2 = QFontDatabase.addApplicationFont('../data/Шрифт2.ttf')
+        families = QFontDatabase.applicationFontFamilies(id2)
         self.pushButton2 = QPushButton('LEVEL I', self)
         self.pushButton2.resize(350, 60)
         self.pushButton2.setFont(QFont(families[0], 30))
         self.pushButton2.setStyleSheet('border: 1px solid black; border-radius: 5px; text-align: center;')
-        self.pushButton2.move(225, 380)
+        self.pushButton2.move(225, 400)
         self.pushButton3 = QPushButton('LEVEL II', self)
         self.pushButton3.resize(350, 60)
         self.pushButton3.setFont(QFont(families[0], 30))
         self.pushButton3.setStyleSheet('border: 1px solid black; border-radius: 5px;')
-        self.pushButton3.move(225, 450)
+        self.pushButton3.move(225, 470)
         self.pushButton4 = QPushButton('LEVEL III', self)
         self.pushButton4.resize(350, 60)
         self.pushButton4.setFont(QFont(families[0], 30))
         self.pushButton4.setStyleSheet('border: 1px solid black; border-radius: 5px;')
-        self.pushButton4.move(225, 520)
-        self.pushButton5 = QPushButton('LEVEL IV', self)
-        self.pushButton5.resize(350, 60)
-        self.pushButton5.setFont(QFont(families[0], 30))
-        self.pushButton5.setStyleSheet('border: 1px solid black; border-radius: 5px;')
-        self.pushButton5.move(225, 590)
+        self.pushButton4.move(225, 540)
         self.font = self.pushButton2.font()
         self.font.setBold(True)
         self.pushButton2.setFont(self.font)
         self.pushButton3.setFont(self.font)
         self.pushButton4.setFont(self.font)
-        self.pushButton5.setFont(self.font)
         self.pushButton2.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.pushButton3.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.pushButton4.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         self.pushButton5.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
-        self.pushButton6.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
 
     def settings(self):
         pass
 
     def termination(self):
+        try:
+            command = 'taskkill /IM Python.exe'
+            os.system(command)
+        except Exception:
+            pass
         self.close()
 
     def info(self):
-        pass
+        subprocess.Popen(['python', 'information.py'])
 
 
 if __name__ == '__main__':
