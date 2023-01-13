90cdd20640c81304eabab4ce1a706923ef7f3b10
Coldi_Ce <rj.kudinov@gmail.com>
2023-01-14 03:13:23 +0500

    exit #4

--- character.py
+++ character.py
@@ -4,8 +4,6 @@ import random
 import pygame
 from config import *
 
-
-
 time = 0
 pygame.display.set_caption("aaa-a-lab")
 pygame.init()
@@ -15,9 +13,10 @@ FPS = 60
 speed = 2
 pygame.mixer.init()
 pygame.mixer.music.load('The_past.mp3')
-pygame.mixer.music.set_volume(0.4)
+pygame.mixer.music.set_volume(0.02)
 pygame.mixer.music.play(loops=-1)
 move_right = move_left = move_up = move_down = False
+flag = False
 
 
 def load_block(name, color_key=None):
@@ -32,6 +31,7 @@ def load_block(name, color_key=None):
         image = image.convert_alpha()
     image = pygame.transform.smoothscale(image, (39, 39))
     return image
+
 
 class Board:
     # создание поля
@@ -84,6 +84,7 @@ def load_img(name, color_key=None):
     image = pygame.transform.smoothscale(image, (33, 33))
     return image
 
+
 class Block(pygame.sprite.Sprite):
     last_update = pygame.time.get_ticks()
     image1 = load_block("pixil-frame-0_3.png")
@@ -102,6 +103,8 @@ for i in range(20):
 
 for i in range(20):
     all_blocks.add(Block(i * 40, 80))
+
+
 class Hero(pygame.sprite.Sprite):
     image1 = load_img(f"cha{0}.png")
     image2 = load_img(f"cha{1}.png")
@@ -128,7 +131,8 @@ class Hero(pygame.sprite.Sprite):
         if pygame.sprite.spritecollideany(self, all_blocks):
             self.rect.x = self.start[0]
             self.rect.y = self.start[1]
-
+        if pygame.sprite.spritecollideany(self, exit1):
+            flag = True
 
     def rotate(self):
         now = pygame.time.get_ticks()
@@ -145,6 +149,24 @@ clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
 
 all_sprites.add(Hero(200, 200))
+
+exit1 = pygame.sprite.Group()
+
+
+class Exit1(pygame.sprite.Sprite):
+    image1 = load_img("door.png")
+    image1 = pygame.transform.smoothscale(image1, (20, 40))
+
+    def __init__(self):
+        super().__init__()
+        self.image = self.image1
+        self.rect = self.image.get_rect()
+        self.rect.x = 780
+        self.rect.y = 200
+
+
+exit1.add(Exit1())
+
 board = Board(20, 20)
 running = True
 while running:
@@ -177,6 +199,7 @@ while running:
     screen.fill(pygame.Color('black'))
     board.render(screen)
     all_blocks.draw(screen)
+    exit1.draw(screen)
     all_sprites.update()
     all_sprites.draw(screen)
     clock.tick(FPS)
