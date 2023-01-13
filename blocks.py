77da77fb519da3126d42383b25c3843a5039f30e
Coldi_Ce <rj.kudinov@gmail.com>
2023-01-14 01:58:08 +0500

    blocks with touch #3

--- blocks.py
+++ blocks.py
@@ -4,34 +4,11 @@ import random
 import pygame
 from config import *
 
-
-
-time = 0
-pygame.display.set_caption("aaa-a-lab")
 pygame.init()
 size = width, height = 800, 800
 screen = pygame.display.set_mode(size)
 FPS = 60
-speed = 2
-pygame.mixer.init()
-pygame.mixer.music.load('The_past.mp3')
-pygame.mixer.music.set_volume(0.4)
-pygame.mixer.music.play(loops=-1)
-move_right = move_left = move_up = move_down = False
 
-
-def load_block(name, color_key=None):
-    image = pygame.image.load(name)
-
-    if color_key is not None:
-        image.convert()
-        if color_key == -1:
-            color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key)
-    else:
-        image = image.convert_alpha()
-    image = pygame.transform.smoothscale(image, (39, 39))
-    return image
 
 class Board:
     # создание поля
@@ -60,6 +37,10 @@ class Board:
                 else:
                     pygame.draw.rect(screen, pygame.Color('white'), (x, y, self.cell_size, self.cell_size), width=1)
 
+    def get_click(self, pos):
+        cell = self.get_cell(pos)
+        self.on_click(cell)
+
     def get_cell(self, pos):
         x, y = pos
         if x < self.left or x > self.left + self.cell_size * self.width \
@@ -70,8 +51,16 @@ class Board:
             cell_y = (y - self.top) // self.cell_size
             return cell_x, cell_y
 
+    def on_click(self, cell):
+        if cell == None:
+            print("None")
+        else:
+            cell_x, cell_y = cell
+            print(cell_x, cell_y)
+            self.board[cell_y][cell_x] = 0 if self.board[cell_y][cell_x] else 1
 
-def load_img(name, color_key=None):
+
+def load_block(name, color_key=None):
     image = pygame.image.load(name)
 
     if color_key is not None:
@@ -81,8 +70,12 @@ def load_img(name, color_key=None):
         image.set_colorkey(color_key)
     else:
         image = image.convert_alpha()
-    image = pygame.transform.smoothscale(image, (33, 33))
+    image = pygame.transform.smoothscale(image, (39, 39))
     return image
+
+
+clock = pygame.time.Clock()
+
 
 class Block(pygame.sprite.Sprite):
     last_update = pygame.time.get_ticks()
@@ -102,83 +95,23 @@ for i in range(20):
 
 for i in range(20):
     all_blocks.add(Block(i * 40, 80))
-class Hero(pygame.sprite.Sprite):
-    image1 = load_img(f"cha{0}.png")
-    image2 = load_img(f"cha{1}.png")
-    last_update = pygame.time.get_ticks()
 
-    def __init__(self, x, y):
-        super().__init__()
-        self.start = [x, y]
-        self.image = self.image1
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-
-    def update(self):
-        if move_up and self.rect.y >= 4:
-            self.rect.y -= speed
-        if move_down and self.rect.y <= 760:
-            self.rect.y += speed
-        if move_left and self.rect.x >= -3:
-            self.rect.x -= speed
-        if move_right and self.rect.x <= 770:
-            self.rect.x += speed
-        self.rotate()
-        if pygame.sprite.spritecollideany(self, all_blocks):
-            self.rect.x = self.start[0]
-            self.rect.y = self.start[1]
-
-
-    def rotate(self):
-        now = pygame.time.get_ticks()
-        if now - self.last_update > 550:
-            self.last_update = now
-            if self.image == self.image1:
-                self.image = self.image2
-            elif self.image == self.image2:
-                self.image = self.image1
-
-
-clock = pygame.time.Clock()
-
-all_sprites = pygame.sprite.Group()
-
-all_sprites.add(Hero(200, 200))
 board = Board(20, 20)
 running = True
 while running:
     time = 0
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d:
-                move_right = True
-            if event.key == pygame.K_w:
-                move_up = True
-            if event.key == pygame.K_s:
-                move_down = True
-            if event.key == pygame.K_a:
-                move_left = True
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_d:
-                move_right = False
-            if event.key == pygame.K_w:
-                move_up = False
-            if event.key == pygame.K_s:
-                move_down = False
-            if event.key == pygame.K_a:
-                move_left = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 pass
 
     screen.fill(pygame.Color('black'))
+    time += 1
     board.render(screen)
     all_blocks.draw(screen)
-    all_sprites.update()
-    all_sprites.draw(screen)
     clock.tick(FPS)
     pygame.display.flip()
 
